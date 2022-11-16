f06ada0ba8dfcbb3b2c2e3f22de9f13ca3cd5119
André Marques <82080573+andremfdev@users.noreply.github.com>
2022-11-16 00:25:58 -0300

    add ajustes no menu mobile

--- main.js
+++ main.js
@@ -17,6 +17,16 @@
 // btnMobile.addEventListener("click", toggleMenu)
 // btnMobile.addEventListener("touchstart", toggleMenu)
 
+const menuDiv = document.getElementById("menu-mobile")
+const btnAnimar = document.getElementById("btn-menu")
+
+menuDiv.addEventListener("click", animarMenu)
+
+function animarMenu() {
+  menuDiv.classList.toggle("abrir")
+  btnAnimar.classList.toggle("ativo")
+}
+
 function createGame(group, player1, hour, player2) {
   return `
   <li>
