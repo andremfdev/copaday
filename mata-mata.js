43d3fdefd092ca67fc98fb8025bce7cd68b06619
andremfdev <andremf.dev@gmail.com>
2022-12-07 10:41:41 -0300

    add results matchday-06 ajuste

--- mata-mata.js
+++ mata-mata.js
@@ -48,46 +48,46 @@ document.querySelector("#cards").innerHTML =
     "03/12",
     "sábado",
     createGame("Oitavas de Final", "Holanda", "3 X 1", "Estados Unidos") +
-    createGame("Oitavas de Final", "Argentina", "2 X 1", "Austrália")
+      createGame("Oitavas de Final", "Argentina", "2 X 1", "Austrália")
   ) +
   createCard(
     "04/12",
     "domingo",
     createGame("Oitavas de Final", "França", "3 x 1", "Polônia") +
-    createGame("Oitavas de Final", "Inglaterra", "3 X 0", "Senegal")
+      createGame("Oitavas de Final", "Inglaterra", "3 X 0", "Senegal")
   ) +
   createCard(
     "05/12",
     "segunda",
-    createGame("Oitavas de Final", "Japão", "1(1) X 1(3)", "Croácia") +
-    createGame("Oitavas de Final", "Brasil", "4 x 1", "Coréia do Sul")
+    createGame("Oitavas de Final", "Japão", "(1)1 X 1(3)", "Croácia") +
+      createGame("Oitavas de Final", "Brasil", "4 x 1", "Coréia do Sul")
   ) +
   createCard(
     "06/12",
     "terça",
-    createGame("Oitavas de Final", "Marrocos", "0(3) X 0(0)", "Espanha") +
-    createGame("Oitavas de Final", "Portugal", "6 X 1", "Suíça")
+    createGame("Oitavas de Final", "Marrocos", "(3)0 X 0(0)", "Espanha") +
+      createGame("Oitavas de Final", "Portugal", "6 X 1", "Suíça")
   ) +
   createCard(
     "09/12",
     "sexta",
     createGame("Quartas de Final", "Croácia", "12:00h", "Brasil") +
-    createGame("Quartas de Final", "Holanda", "16:00h", "Argentina")
+      createGame("Quartas de Final", "Holanda", "16:00h", "Argentina")
   ) +
   createCard(
     "10/12",
     "sábado",
     createGame("Quartas de Final", "Marrocos", "12:00h", "Portugal") +
-    createGame("Quartas de Final", "Inglaterra", "16:00h", "França")
-
+      createGame("Quartas de Final", "Inglaterra", "16:00h", "França")
+  )
 function scrollToTop() {
-      window.scrollTo(0, 0)
-    }
+  window.scrollTo(0, 0)
+}
 
 window.addEventListener("scroll", function () {
-      let scroll = this.document.querySelector(".scrollTop")
-      scroll.classList.toggle("active", window.scrollY > 450)
-    })
+  let scroll = this.document.querySelector(".scrollTop")
+  scroll.classList.toggle("active", window.scrollY > 450)
+})
 
 const menuItens = document.querySelectorAll('.navigation a[href^="#"]')
 
